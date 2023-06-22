fd5b58d8222361b0d8d00874856558480993ccb1
Zabieruwu <megajavier2000@gmail.com>
2023-06-21 19:34:52 -0600

    se ven las imagenes
    se añadieron todas las img del flashcard

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,14 +13,14 @@ export class Tab1Page {
   showImage: boolean[] = [];
 
   herramientas = [
-    { nombre: 'Llave de tubo', descripcion: 'Herramienta utilizada para apretar o aflojar tuercas y pernos.', imagen:'tabs/tab1/img/llavedetubo.jpg' },
-    { nombre: 'Destornillador', descripcion: 'Herramienta con una punta en forma de hoja que se utiliza para girar y ajustar tornillos.',imagen:'img/llavedetubo.jpg' },
-    { nombre: 'Alicate', descripcion: 'Herramienta de agarre que se utiliza para sujetar, torcer o cortar objetos como alambres.',imagen:'img/llavedetubo.jpg' },
-    { nombre: 'Compresor de aire', descripcion: 'Utilizado para generar y almacenar aire comprimido.',imagen:'img/llavedetubo.jpg' },
-    { nombre: 'Multímetro', descripcion: 'Herramienta de medición electrónica utilizada para medir voltaje, corriente y resistencia eléctrica.',imagen:'img/llavedetubo.jpg' },
-    { nombre: 'Gato hidráulico', descripcion: 'Dispositivo utilizado para elevar vehículos y facilitar la reparación en la parte inferior del automóvil.',imagen:'img/llavedetubo.jpg'},
-    { nombre: 'Llave de impacto', descripcion: 'Herramienta eléctrica utilizada para apretar o aflojar pernos y tuercas con alta potencia y torque.',imagen:'img/llavedetubo.jpg' },
-    { nombre: 'Extractor de rodamientos', descripcion: 'Se usa para extraer rodamientos, poleas u otros componentes ajustados y difíciles de quitar.',imagen:'img/llavedetubo.jpg' }
+    { nombre: 'Llave de tubo', descripcion: 'Herramienta utilizada para apretar o aflojar tuercas y pernos.', imagen: 'assets/img/llavedetubo.jpg'},
+    { nombre: 'Destornillador', descripcion: 'Herramienta con una punta en forma de hoja que se utiliza para girar y ajustar tornillos.',imagen:'assets/img/destornillador.jpg' },
+    { nombre: 'Alicate', descripcion: 'Herramienta de agarre que se utiliza para sujetar, torcer o cortar objetos como alambres.',imagen:'assets/img/alicate.jpg' },
+    { nombre: 'Compresor de aire', descripcion: 'Utilizado para generar y almacenar aire comprimido.',imagen:'assets/img/compresordeaire.jpg' },
+    { nombre: 'Multímetro', descripcion: 'Herramienta de medición electrónica utilizada para medir voltaje, corriente y resistencia eléctrica.',imagen:'assets/img/multimetro.jpg' },
+    { nombre: 'Gato hidráulico', descripcion: 'Dispositivo utilizado para elevar vehículos y facilitar la reparación en la parte inferior del automóvil.',imagen:'assets/img/gato.jpg'},
+    { nombre: 'Llave de impacto', descripcion: 'Herramienta eléctrica utilizada para apretar o aflojar pernos y tuercas con alta potencia y torque.',imagen:'assets/img/llavedeimpacto.jpg' },
+    { nombre: 'Extractor de rodamientos', descripcion: 'Se usa para extraer rodamientos, poleas u otros componentes ajustados y difíciles de quitar.',imagen:'assets/img/extractorderodamiento.jpg' }
   ];
 
   flipCard(index: number): void {
